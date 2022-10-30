93b46fcc2a74bec5d3a1c396895bd73987f4e53d
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-30 08:41:38 -0700

    added content-container and menu-bar elements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import './style.css';
 
+/*
+* menu bar
+*/
+
 let body = document.querySelector('body');
+
+let menu_bar = document.createElement('div');
+body.appendChild(menu_bar);
 
 let home_button = document.createElement('button');
 let menu_button = document.createElement('button');
 let contact_button = document.createElement('button');
 
-body.appendChild(home_button);
-body.appendChild(menu_button);
-body.appendChild(contact_button);
+home_button.textContent='home';
+menu_button.textContent='menu';
+contact_button.textContent='contact';
+
+menu_bar.appendChild(home_button);
+menu_bar.appendChild(menu_button);
+menu_bar.appendChild(contact_button);
+
+/*
+* content
+*/
+
+let content = document.createElement('div');
+content.setAttribute('class','content-container')
+body.appendChild(content);
